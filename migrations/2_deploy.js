63342defa8f7d2381fc70cd15fb9bcdb634d85fd
Andrew Starling <55167778+tobogganhill@users.noreply.github.com>
2021-03-07 16:25:36 -0500

    Add files via upload

---
+++ migrations/2_deploy.js
@@ -0,0 +1,15 @@
+// migrations/2_deploy.js
+// SPDX-License-Identifier: MIT
+
+const ERC721PresetMinterPauserAutoId = artifacts.require(
+	'ERC721PresetMinterPauserAutoId'
+);
+
+module.exports = function(deployer) {
+	deployer.deploy(
+		ERC721PresetMinterPauserAutoId,
+		'Artcryption',
+		'ART',
+		'https://my-json-server.typicode.com/abcoathup/samplenft/tokens/'
+	);
+};
